8e2ac540eacae234e83272a6432fffaaf0fea4ee
Aaron Allen <hello@aaronmallen.me>
2025-02-11 01:39:31 -0600

    initial commit

---
+++ .config/mise/env.zsh
@@ -0,0 +1,7 @@
+[ -f "$XDG_CONFIG_HOME/rust/env.zsh" ] && source "$XDG_CONFIG_HOME/rust/env.zsh"
+
+export MISE_CARGO_HOME=$CARGO_HOME
+export MISE_NODE_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/javascript/default_npm_packages
+export MISE_PYTHON_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/python/default_packages
+export MISE_RUBY_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/ruby/default_gems
+export MISE_RUSTUP_HOME=$RUSTUP_HOME
