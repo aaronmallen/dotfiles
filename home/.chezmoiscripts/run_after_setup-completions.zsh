9c2619bf0c8152cd0bc1a9fb6f582e593d16efc7
Aaron Allen <hello@aaronmallen.me>
2026-02-19 10:57:07 -0600

    Add zsh completions for git-lfs, mas, and tailscale

--- home/.chezmoiscripts/run_after_setup-completions.zsh
+++ home/.chezmoiscripts/run_after_setup-completions.zsh
@@ -12,14 +12,17 @@ command -v deno      >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/deno      && deno
 command -v docker    >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/docker    && docker completion zsh > ${XDG_CONFIG_HOME}/docker/_docker
 command -v doctl     >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/doctl     && doctl completion zsh > ${XDG_CONFIG_HOME}/doctl/_doctl
 command -v gh        >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/gh        && gh completion --shell zsh > ${XDG_CONFIG_HOME}/gh/_gh
+command -v git-lfs   >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/git-lfs   && git lfs completion zsh > ${XDG_CONFIG_HOME}/git-lfs/_git-lfs
 command -v jj        >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/jj        && jj util completion zsh > ${XDG_CONFIG_HOME}/jj/_jj
 command -v kubectl   >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/kubectl   && kubectl completion zsh > ${XDG_CONFIG_HOME}/kubectl/_kubectl
+command -v mas       >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/mas       && mas --generate-completion-script zsh > ${XDG_CONFIG_HOME}/mas/_mas
 command -v minikube  >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/minikube  && minikube completion zsh > ${XDG_CONFIG_HOME}/minikube/_minikube
 command -v mise      >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/mise      && mise completion zsh > ${XDG_CONFIG_HOME}/mise/_mise
 command -v op        >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/op        && op completion zsh > ${XDG_CONFIG_HOME}/op/_op
 command -v opencode  >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/opencode  && opencode completion zsh > ${XDG_CONFIG_HOME}/opencode/_opencode
 command -v rg        >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/ripgrep   && rg --generate complete-zsh > ${XDG_CONFIG_HOME}/ripgrep/_rg
 command -v starship  >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/starship  && starship completions zsh > ${XDG_CONFIG_HOME}/starship/_starship
+command -v tailscale >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/tailscale && tailscale completion zsh > ${XDG_CONFIG_HOME}/tailscale/_tailscale
 command -v watchexec >/dev/null && mkdir -p ${XDG_CONFIG_HOME}/watchexec && watchexec --completions zsh > ${XDG_CONFIG_HOME}/watchexec/_watchexec
 
 exit 0
