d7e42317b3e845d779ee4ec90614145be41107ad
Aaron Allen <hello@aaronmallen.me>
2026-03-25 16:44:04 -0500

    Add ATUIN_LOGS__DIR env var

---
+++ home/private_dot_config/private_atuin/private_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+export ATUIN_LOGS__DIR="${XDG_STATE_HOME}/atuin/logs"
--- home/private_dot_config/private_atuin/private_init.zsh
+++ home/private_dot_config/private_atuin/private_init.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-_load_zsh_topic "atuin" --deps mise
+_load_zsh_topic "atuin" --source-env --deps mise
 eval "$(atuin init zsh)"
 eval "$(atuin ai init)"
